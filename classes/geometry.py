c75d54c26e18f31fed2bb28a2683f5b3381e01dd
spnya <alexander-ukhanov@yandex.ru>
2022-08-22 23:10:09 +0300

    geometry class and new picture

---
+++ classes/geometry.py
@@ -0,0 +1,6 @@
+from math import sqrt
+
+class geometry:
+    def distance(x1, y1, x2, y2):
+        
+        return sqrt((x1-x2)**2 + (y1-y2)**2)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PIL import Image, ImageDraw
 from random import randint
 
+from classes.geometry import *
+
 width = 1980
 hight = 1080
 
@@ -8,7 +10,7 @@ img = Image.new('RGB', (width, hight), color = (230, 235, 255))
 
 draw = ImageDraw.Draw(img)
 
-n = 30
+n = 40
 
 dots = []
 
@@ -18,15 +20,23 @@ for i in range(n):
     dots.append({
         'x': x,
         'y': y,
-        'links': []
+        'links': [],
+        'cnt': 0
     })
 
 for i in dots:
     draw.point((i['x'], i['y']), 'black')    
 
-for i in dots:
-    for j in dots:
-        if (randint(1, 4) == 1):
-            draw.line((i['x'], i['y'], j['x'], j['y']), fill=1)
+max_node_pow = 4
 
-img.save('./images/img1.png')
+for i in range(len(dots)):
+    while (dots[i]['cnt'] < max_node_pow):
+        for j in range(i+1, len(dots)):
+            if (dots[j]['cnt'] > max_node_pow or dots[i]['cnt'] > max_node_pow):
+                continue
+            if (randint(1, 1) == 1 and geometry.distance(dots[i]['x'], dots[i]['y'], dots[j]['x'], dots[j]['y']) <= randint(500, 800)):
+                draw.line((dots[i]['x'], dots[i]['y'], dots[j]['x'], dots[j]['y']), fill=1)
+                dots[i]['cnt'] += 1
+                dots[j]['cnt'] += 1
+        dots[i]['cnt'] += 1
+img.save('./images/img3.png')
